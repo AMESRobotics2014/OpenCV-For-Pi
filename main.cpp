2e576c6044e1201c42765ac6cba46d74a843ef42
root <root@raspberrypi.(none)>
2014-02-05 01:56:18 +0000

    updated code: we drew dots on corners and have started with area calculations. Happy Birthday Kolton!!!!!!!!

--- main.cpp
+++ main.cpp
@@ -62,9 +62,9 @@ int main()
 	key = cvWaitKey(10);
 
 	cvCvtColor(frame,gray,CV_BGR2GRAY);
-	cvThreshold(gray,gray,32,255,CV_THRESH_BINARY);
+	cvThreshold(gray,gray,64,255,CV_THRESH_BINARY);
 
-	cvShowImage("Camera_Output2", gray);
+	//cvShowImage("Camera_Output2", gray);
 
 	cvFindContours(gray, storage,&contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));	
 	
@@ -79,24 +79,50 @@ int main()
 		    pt[i] = (CvPoint*)cvGetSeqElem(result, i);
 		}
 		
+		//cout << pt[0]->x<< ", "  << pt[0]->y << endl;
+		//cout << pt[1]->x<< ", "  << pt[1]->y << endl;
+		//cout << pt[2]->x<< ", "  << pt[2]->y << endl;
+		//cout << pt[3]->x<< ", "  << pt[3]->y << endl;
 		
 		
-		//cout << pt[0]->x<<endl;
-		//cout << *pt[1]<<endl;
-		//cout << *pt[2]<<endl;
-		//cout << *pt[3]<<endl;
-		
-		//cvLine(frame, *pt[0], *pt[1], cvScalar(0, 255, 0), 4);
-		//cvLine(frame, *pt[1], *pt[2], cvScalar(0, 255, 0), 4);
-		//cvLine(frame, *pt[2], *pt[3], cvScalar(0, 255, 0), 4);
-		//cvLine(frame, *pt[3], *pt[0], cvScalar(0, 255, 0), 4);
+	        //float heightOne = pt[3]->y - pt[0]->y;
+		//float hieghtTwo = pt[2]->y - pt[1]->y;
+		//float side = (hieghtOne + hieghtTwo)/2;
 
-		cvBoundingRect(pt,0);
+	        float sidethree = pt[1]->x - pt[0]->x;
+		//float sidefour = pt[3]->x - pt[2]->x;
+		//float otherside = (sidethree + sidefour)/2;
+		//cout <<"hieght:"<< hieghtOne <<endl;
+		//cout <<"Length 2"<< otherside <<endl;
+           	
+		
+		
+		float sideleft = pt[1] -> y - pt[0] -> y;
+		float sideright = pt[3] -> y - pt[2] ->y;
+		float  height =(sideleft + sideright)/2;
+
+		float sidetop = pt[2] -> x - pt[0] -> x;
+		float sidebottom = pt[3] -> x - pt[1] -> x;
+		float  width =(sidetop + sidebottom)/2;
+
+		float area = height * width;
+		if(area>x){
+		float areavert = area;
+		}else{
+		float areahori = area;
+		}
+
+		cvLine(frame, *pt[0], *pt[0], cvScalar(0, 255, 0), 4);
+		cvLine(frame, *pt[1], *pt[1], cvScalar(0, 255, 0), 4);
+		cvLine(frame, *pt[2], *pt[2], cvScalar(0, 255, 0), 4);
+		cvLine(frame, *pt[3], *pt[3], cvScalar(0, 255, 0), 4);
 
 	   }
 	    contours = contours->h_next;
+//cvShowImage("Camera_Output2", gray);
+	float totarea = areavert + areahori;
 	}
-	
+cvShowImage("Camera_Output2", frame);
 
 	if (char(key) == 27) {
 	 // cvDestroyAllWindows();
@@ -106,7 +132,7 @@ int main()
 	   break;	
 	}
 
-	if (char(key) == 99) {
+ 	if (char(key) == 99) {
 	   //counter++;
 	   
 	   cvSaveImage("Im age Bro.pgm", gray);	
