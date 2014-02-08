80fcac341f8b758bfc2f0f2a7fada44922e66b2c
root <root@raspberrypi.(none)>
2014-02-08 22:21:01 +0000

    updated: added some stuff for finding which point is which

--- main.cpp
+++ main.cpp
@@ -15,19 +15,22 @@
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 using namespace std;
 using namespace cv;
 char key;
 
-boolean hot;
+bool hot;
 
-int auto(a,b)
+bool autonomous (int a,int b)
 {
 	float area = a * b;
-	
+	hot = false;
+	//cout << area << endl;
+
 	if(area > 450 && area < 640) {
-	cout << "Hot" << endl;
+	//cout << "Hot" << endl;
 	hot = true;
 	}
 /*
@@ -35,12 +38,25 @@ int auto(a,b)
 	cout << area << endl;
 	}
 */
+	return hot;
+}
+
+double teleop (float a, float b)
+{
+	double distance;
+
+ 	if(a<b) {
+
+	distance = 44.099407801909 * pow(0.91175211621759,a);
+
+}
+
+	return distance;
 }
 
 int main()
 {
-   hot = false;
-   //VideoCapture imgcapture(CV_CAP_ANY);
+    //VideoCapture imgcapture(CV_CAP_ANY);
    cvNamedWindow("Camera_Output2", 1);
    CvCapture* capture = cvCreateCameraCapture(0);
 	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 320);
@@ -59,7 +75,7 @@ int main()
 	key = cvWaitKey(10);
 
 	cvCvtColor(frame,gray,CV_BGR2GRAY);
-	cvThreshold(gray,gray,63,255,CV_THRESH_BINARY);
+	cvThreshold(gray,gray,32,255,CV_THRESH_BINARY);
 
 	//cvShowImage("Camera_Output2", gray);
 
@@ -70,37 +86,75 @@ int main()
 	   if (result -> total == 4) {
 
 		CvPoint *pt[4];
-
+		CvPoint *npt[4];
 	     	for (int i = 0; i < 4; i++) {
 		    pt[i] = (CvPoint*)cvGetSeqElem(result, i);
 		}
 
-		//cout << pt[0]->x<< ", "  << pt[0]->y << endl;
-		//cout << pt[1]->x<< ", "  << pt[1]->y << endl;
-		//cout << pt[2]->x<< ", "  << pt[2]->y << endl;
-		//cout << pt[3]->x<< ", "  << pt[3]->y << endl;
-
-		float sideleft = pt[1] -> y - pt[0] -> y;
+		//cout <<"pt 0: " << pt[0]->x<< ", "  << pt[0]->y << endl;
+		//cout <<"pt 1: " << pt[1]->x<< ", "  << pt[1]->y << endl;
+		//cout <<"pt 2: " << pt[2]->x<< ", "  << pt[2]->y << endl;
+		//cout <<"pt 3: " << pt[3]->x<< ", "  << pt[3]->y << endl;
+/*
+		float sideleft = pt[1] -> y - pt[0] -> y;	
 		float sideright = pt[2] -> y - pt[3] -> y;
+		sideleft = abs(sideleft);
+		cout<<"Side left: " <<sideleft<<endl;
+		sideright = abs(sideright);
+		cout<<"Side right: " <<sideright<<endl;
 		float  height = (sideleft + sideright)/2;
 
 		float sidetop = pt[2] -> x - pt[0] -> x;
 		float sidebottom = pt[3] -> x - pt[1] -> x;
 		float  width = (sidetop + sidebottom)/2;
-/*
-		if(height > 5) {
-		cout << height <<endl;
-}
-		if(width > 2) {
-		cout << width << endl;
-}
 */
-		cvLine(frame, *pt[0], *pt[1], cvScalar(0, 255, 0), 4);
-		cvLine(frame, *pt[1], *pt[2], cvScalar(0, 255, 0), 4);
-		cvLine(frame, *pt[2], *pt[3], cvScalar(0, 255, 0), 4);
-		cvLine(frame, *pt[3], *pt[0], cvScalar(0, 255, 0), 4);
 
-		hot = auto(height, width);
+avgy = (pt[0]->y + pt[2]->y + pt[3]->y + pt[4]->y)/4;
+avgx = (pt[0]->x + pt[2]->x + pt[3]->x + pt[4]->x)/4;
+
+
+for(int a = 0;a < 4;a++)
+{
+	if(pt[a]->y > avgy && pt[a]->x > avgx) {
+	npt[0]=pt[a];
+}
+	if(pt[a]->y > avgy && pt[a]->x > avgx) {
+	npt[1]=pt[a];
+}
+	if(pt[a]->y > avgy && pt[a]->x > avgx) {
+	npt[2] = pt[a];
+}
+	if(pt[a]->y > avgy && pt[a]->x > avgx) {
+	npt[3] = pt[a];
+}
+
+}
+
+//		if(height > 0) {
+//		cout << height <<endl;
+//}
+
+//		if(width > 0) {
+//		cout << width << endl;
+//}
+
+
+		cvLine(frame, *pt[0], *pt[1], cvScalar(0, 255, 0), 4);
+		//cvLine(frame, *pt[1], *pt[2], cvScalar(0, 255, 0), 4);
+		//cvLine(frame, *pt[2], *pt[3], cvScalar(0, 255, 0), 4);
+		//cvLine(frame, *pt[3], *pt[0], cvScalar(0, 255, 0), 4);
+
+		hot = autonomous(height, width);
+
+		if(hot == true) {
+		cout << "Goal is Hot" << endl;
+		}
+
+		double distancefinal = teleop(height, width);
+		//cout << distancefinal << endl;
+
+
+
 	   }
 	    	contours = contours->h_next;
 
